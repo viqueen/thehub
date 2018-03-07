9e94f33e099f6f3b79509bea72fabc3589fea23d
viqueen <hrehioui@atlassian.com>
2018-03-07 07:22:46 +0100

    i18n: to the backend too

--- portal-server/src/main/java/org/viqueen/portal/dev/CustomersApiMock.java
+++ portal-server/src/main/java/org/viqueen/portal/dev/CustomersApiMock.java
@@ -8,17 +8,25 @@ import org.springframework.stereotype.Component;
 import org.viqueen.portal.api.CustomersApi;
 import org.viqueen.portal.model.Customer;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Locale;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 @Profile("dev")
 @Component("customersApi")
 public class CustomersApiMock implements CustomersApi {
 
-    private final Faker faker;
+    private final FakerSupplier fakerSupplier;
+    private final HttpServletRequest request;
 
     @Autowired
-    public CustomersApiMock() {
-        faker = new Faker();
+    public CustomersApiMock(
+            final FakerSupplier fakerSupplier,
+            final HttpServletRequest request) {
+        this.fakerSupplier = fakerSupplier;
+        this.request = request;
     }
 
     @Override
@@ -28,13 +36,27 @@ public class CustomersApiMock implements CustomersApi {
 
     @Override
     public ResponseEntity<Customer> getCustomerById(String customerId) {
+        // hax
+        final Locale locale = request.getLocale();
+        final Locale resolvedLocale;
+        if (isEmpty(locale.getCountry()) && "nb".equals(locale.getLanguage())) {
+            // faker does not know how to resolve locales without a country defined
+            // while I fix that (open-source PR), I am applying this workaround for Norsk
+            resolvedLocale = Locale.forLanguageTag("nb-NO");
+        } else {
+            resolvedLocale = request.getLocale();
+        }
+
+        final Faker faker = fakerSupplier.get(resolvedLocale);
         final Customer customer = new Customer();
+
         customer.customerId(customerId);
         customer.companyName(faker.company().name());
         customer.countryOfBirth(faker.address().country());
         customer.customerType(Customer.CustomerTypeEnum.PERSON);
         customer.firstName(faker.name().firstName());
         customer.lastName(faker.name().lastName());
+
         return ResponseEntity.ok(customer);
     }
 
---
+++ portal-server/src/main/java/org/viqueen/portal/dev/FakerSupplier.java
@@ -0,0 +1,15 @@
+package org.viqueen.portal.dev;
+
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class FakerSupplier {
+
+    public Faker get(final Locale locale) {
+        return Faker.instance(locale);
+    }
+
+}
