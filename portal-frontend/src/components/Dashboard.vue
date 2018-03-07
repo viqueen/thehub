c186d776557fa68d23ac3508f3266ebda643500c
viqueen <hrehioui@atlassian.com>
2018-03-07 01:04:09 +0100

    ui: initial ui using vue material design components

---
+++ portal-frontend/src/components/Dashboard.vue
@@ -0,0 +1,57 @@
+<template>
+  <md-app md-waterfall md-mode="fixed">
+    <md-app-toolbar class="md-primary">
+      <span class="md-title">{{ api.title }}</span>
+    </md-app-toolbar>
+    <md-app-drawer md-permanent="full">
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <h2>API Browser</h2>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon>
+          <span class="md-list-item-text">Inbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon>
+          <span class="md-list-item-text">Sent Mail</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon>
+          <span class="md-list-item-text">Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon>
+          <span class="md-list-item-text">Spam</span>
+        </md-list-item>
+      </md-list>
+    </md-app-drawer>
+  </md-app>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data () {
+    return {
+      api: {
+        title: 'mock api'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .md-app {
+    border: 1px solid rgba(#000, .12);
+  }
+  .md-drawer {
+    width: 300px;
+    max-width: calc(100vw - 125px);
+  }
+</style>
--- portal-frontend/src/router/index.js
+++ portal-frontend/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+// import 'vue-material/dist/theme/default-dark.css'
+import 'vue-material/dist/theme/black-green-dark.css'
 
+Vue.use(VueMaterial)
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
